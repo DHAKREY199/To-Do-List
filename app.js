6757cc70e59d87c1a01da4718da94ab219682d28
Himanshu Dhakrey <himanshudhakrey2014@gmail.com>
2017-07-22 10:57:07 +0000

    Added Database

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
 var express = require("express"),
     app     = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
     
-
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
+    
+mongoose.connect("mongodb://dhakrey:ghanta@ds143181.mlab.com:43181/todolist");
+var todolist = new mongoose.Schema({
+    task : String
+});
+var List = mongoose.model("List", todolist);
+// var notebook = new List({task : "Buy Black and Blue pens"});
+// notebook.save(function(err ,list){
+//     if(err)
+//     console.log("Something Wrong");
+//     else {
+//         console.log("We saved item in todolist");
+//         console.log(list);
+//     }
+// });
+
 
 
 app.get("/", function (req, res) {
-    res.render("index");
+        List.find({} , function(err, items){
+            if(err){
+                console.log("Somethig went Wrong");
+            } else {
+                res.render("index",{items : items});
+            }
+        });
+});
+app.post("/add", function(req, res) {
+    var item = req.body.item;
+    //console.log(item);
+    
+    List.create({task : item}, function(err, items){
+          if(err){
+              console.log("Something Wrong");
+          } else {
+              res.redirect("/");
+              
+          }
+    });
 });
 
 
